cb4a7e26a40d9bc6d17a9b2596823a020242daca
gaurav soni <gaurav6642@gmail.com>
2019-01-13 14:21:22 +0530

    newly created git folder

---
+++ main.py
@@ -0,0 +1,17 @@
+Song="Hope"
+Artist="The Chainsmokers,Winona Oak"
+Album="Sick Boy"
+Genre="Rock"
+YearofRelase=2019
+DurationinMinutes=3.00 #DecimalVariable
+
+"""These all are the examples of Varibles Intialization
+   by using detials of a song"""
+
+print(Song)
+print(Artist)
+print(Album)
+print(Genre)
+print(YearofRelase)
+print(DurationinMinutes)
+

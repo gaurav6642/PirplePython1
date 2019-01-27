7a69935d0ec1b85e180037a8e3c1c6fa085db12d
Gaurav <gaurav6642@gmail.com>
2019-01-27 22:38:09 +0530

    Add files via upload

---
+++ Import.py
@@ -0,0 +1,5 @@
+import calendar as c
+
+c.setfirstweekday(1)# this will going to set first week day
+print(c.firstweekday())
+print(c.isleap(2018))# this will going to return true for leap year else return False
